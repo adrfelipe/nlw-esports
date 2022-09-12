1eb7150a54c761dd5a91841fb2eb3c826b900986
André Felipe <adrfelipe.sz@gmail.com>
2022-09-12 19:38:48 -0300

    Aula 01: Configurando ambiente web e mobile

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+
+
+function Button() {
+  return (
+    <button>
+      Enviar
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
+}
+
+export default App
